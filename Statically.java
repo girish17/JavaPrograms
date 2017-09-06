31ed2443add97ad0876858a2ea64b3b91281e850
girish17 <m.girish.cs@gmail.com>
2017-09-06 21:01:19 +0530

    changes

---
+++ Statically.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.math.*;
+
+class Statically
+{
+   private static int num;
+   private static int i;   
+   public static void main(String args[])
+   {
+     for(; i<Integer.parseInt(args[0]); i++)
+     {
+       num++;
+       System.out.println(num);
+     }
+   }
+}
+
